2268eacc72d8b6c515797dc3a76d96cf1cc9219b
keyansheng <65121402+keyansheng@users.noreply.github.com>
2021-07-15 21:55:24 +0800

    Add files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,52 @@
+$PSDefaultParameterValues['*:Encoding'] = 'Default'
+function Get-SymbolicLink { cmd /c dir /al /s | Where-Object { $_ -match 'symlink' } }
+function Get-SymbolicLinkDirectory { cmd /c dir /al /s | Where-Object { $_ -match 'symlinkd' } }
+function Get-Junction { cmd /c dir /al /s | Where-Object { $_ -match 'junction' } }
+function New-SymbolicLink { cmd /c mklink @args }
+function New-SymbolicLinkDirectory { cmd /c mklink /d @args }
+function New-Junction { cmd /c mklink /j @args }
+function Hide-Item { (Get-Item @args).Attributes += 'Hidden' }
+function Show-Item { (Get-Item @args -Force).Attributes -= 'Hidden' }
+function Update-ScoopApps {
+    $ScoopArgs = $args
+    if (!$ScoopArgs) { $ScoopArgs += '*' }
+    scoop update @ScoopArgs
+    scoop cleanup @ScoopArgs -k
+}
+function Set-Journal ([string] $Date = (Get-Date -Format 'yyyy-MM-dd') ) {
+    $JournalPath = Join-Path $env:JOURNAL "$Date.md"
+    $Action = if (Test-Path $JournalPath) { 'Update' } else { 'Add' }
+    vim $JournalPath
+    git -C $env:JOURNAL add $JournalPath
+    git -C $env:JOURNAL commit -m "$Action $Date"
+}
+function Send-Journal { git -C $env:JOURNAL push }
+function Measure-Duration (
+    [string]$EndTime,
+    [string]$StartTime = 0
+) {
+    try {
+        [TimeSpan]::Parse($EndTime).TotalSeconds - [TimeSpan]::Parse($StartTime).TotalSeconds
+    } catch {
+        $EndTime - $StartTime
+    }
+}
+function New-VideoClip (
+    [string] $SourceFile,
+    [string] $StartTime,
+    [string] $EndTime,
+    [string] $DestinationFile = 'video.mp4'
+) {
+    ffmpeg -ss $StartTime -i $SourceFile -t (Measure-Duration $EndTime $StartTime) $DestinationFile -y
+}
+function New-VideoClipYouTube (
+    [string] $Url,
+    [string] $StartTime,
+    [string] $EndTime,
+    [string] $DestinationFile = 'video.mp4',
+    [string] $TempFile = 'temp'
+) {
+    youtube-dl $Url -f bestvideo[ext=webm]+bestaudio[ext=webm] -o $TempFile
+    New-VideoClip "$TempFile.webm" $StartTime $EndTime $DestinationFile
+}
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
