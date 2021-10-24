2b2afb1a19bd0554ff7037f5ad3689235aadc591
keyansheng <65121402+keyansheng@users.noreply.github.com>
2021-10-24 16:36:08 +0800

    Remove New-VideoClip functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,34 +16,6 @@ function Set-Journal ([datetime] $Date = (Get-Date) ) {
     git -C $env:JOURNAL add $JournalPath
     git -C $env:JOURNAL commit -m "$Action $ISODate"
 }
-function Measure-Duration (
-    [string]$EndTime,
-    [string]$StartTime = 0
-) {
-    try {
-        [TimeSpan]::Parse($EndTime).TotalSeconds - [TimeSpan]::Parse($StartTime).TotalSeconds
-    } catch {
-        $EndTime - $StartTime
-    }
-}
-function New-VideoClip (
-    [string] $SourceFile,
-    [string] $StartTime,
-    [string] $EndTime,
-    [string] $DestinationFile = 'video.mp4'
-) {
-    ffmpeg -ss $StartTime -i $SourceFile -t (Measure-Duration $EndTime $StartTime) $DestinationFile
-}
-function New-VideoClipYouTube (
-    [string] $Url,
-    [string] $StartTime,
-    [string] $EndTime,
-    [string] $DestinationFile = 'video.mp4',
-    [string] $TempFile = 'temp'
-) {
-    youtube-dl $Url -f bestvideo[ext=webm]+bestaudio[ext=webm] -o $TempFile
-    New-VideoClip "$TempFile.webm" $StartTime $EndTime $DestinationFile
-}
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -HistorySavePath $env:TEMP\history.ps1
 Set-Alias c Invoke-CMD
